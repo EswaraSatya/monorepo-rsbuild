7e7575be73d01713880bb7995c9539393d6db074
eswar3940 <eswar3940@gmail.com>
2025-04-08 16:43:19 +0530

    latestvitecheck12

--- packages/dashboard/rsbuild.config.ts
+++ packages/dashboard/rsbuild.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from '@rsbuild/core';
+import { join } from 'path';
 
 export default defineConfig({
   html: {
@@ -8,5 +9,9 @@ export default defineConfig({
     entry: {
       main: './src/app/app.tsx', // Ensure this file exists
     },
+    alias: {
+      'app1/Module': join(__dirname, '../../packages/app1/src/remote-entry.ts'),
+      'app2/Module': join(__dirname, '../../packages/app2/src/remote-entry.ts')
+    }
   },
 });
